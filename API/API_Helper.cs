d90772f9cbbff3c5315a99d721c9f29cfc670fc9
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-01-05 14:03:56 +0700

    add helper apis ~01

---
+++ API/API_Helper.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentManagement.Server.API
+{
+    public static class API_Helper
+    {
+        public static WebApplication MapAPI_Helper(this WebApplication app)
+        {
+            app
+                .MapGet("/tinh-trang-hoc-tap/get-all", () =>
+                {
+                    return new List<string>() { "đang học", "thôi học", "tốt nghiệp", "bảo lưu kết quả", "đình chỉ học", };
+                })
+                .WithTags("Helper");
+
+            return app;
+        }
+    }
+}
