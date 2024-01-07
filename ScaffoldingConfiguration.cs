b2f7e42dc5d4f0c5dba598a879e964b73fe4be25
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-01-07 15:32:31 +0700

    disable scaffolding

--- ScaffoldingConfiguration.cs
+++ ScaffoldingConfiguration.cs
@@ -1,27 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.EntityFrameworkCore.Metadata;
+//using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
 
-namespace StudentManagement.Server
-{
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "<Pending>")]
-    public class CustomCandidateNamingService : CandidateNamingService
-    {
-        public override string GetDependentEndCandidateNavigationPropertyName(IReadOnlyForeignKey foreignKey)
-        {
-            string name = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase
-            (foreignKey.Properties[0].GetColumnName().Replace("ma_", string.Empty).Replace('_', ' ')).Replace(" ", string.Empty);
-            return name;
-        }
-    }
+//namespace StudentManagement.Server
+//{
+//    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "<Pending>")]
+//    public class CustomCandidateNamingService : CandidateNamingService
+//    {
+//        public override string GetDependentEndCandidateNavigationPropertyName(IReadOnlyForeignKey foreignKey)
+//        {
+//            string name = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase
+//            (foreignKey.Properties[0].GetColumnName().Replace("ma_", string.Empty).Replace('_', ' ')).Replace(" ", string.Empty);
+//            return name;
+//        }
+//    }
 
-    public class DesignTimeServices : IDesignTimeServices
-    {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "<Pending>")]
-        public void ConfigureDesignTimeServices(IServiceCollection services)
-        {
-            services.AddSingleton<ICandidateNamingService, CustomCandidateNamingService>();
-        }
-    }
-}
+//    public class DesignTimeServices : IDesignTimeServices
+//    {
+//        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "<Pending>")]
+//        public void ConfigureDesignTimeServices(IServiceCollection services)
+//        {
+//            services.AddSingleton<ICandidateNamingService, CustomCandidateNamingService>();
+//        }
+//    }
+//}
